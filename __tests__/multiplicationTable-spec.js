479bb1856270c159ddd154c59962fab3fd6a2414
Amy oyzl <32362887+Amyoyzl@users.noreply.github.com>
2019-07-09 17:33:34 +0800

    add task graph

--- __tests__/multiplicationTable-spec.js
+++ __tests__/multiplicationTable-spec.js
@@ -11,6 +11,28 @@ it('should return null when invoke getMultipicationTable given start bigger than
 
 });
 
+it('should return null when invoke getMultipicationTable given start small than 0', () => {
+    // given
+    const start = -2;
+    const end = 2;
+    // when
+    const result = multipicationTable.getMultipicationTable(start, end);
+    // then
+    expect(result).toBe(null);
+
+});
+
+it('should return null when invoke getMultipicationTable given end large than 1000', () => {
+    // given
+    const start = -2;
+    const end = 1020;
+    // when
+    const result = multipicationTable.getMultipicationTable(start, end);
+    // then
+    expect(result).toBe(null);
+
+});
+
 it('should return a multiplication table when invoke getMultipicationTable given start is 3 and end is 5', () => {
     // given
     const start = 3;
